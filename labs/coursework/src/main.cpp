147f2bb1fc1c98571f2dea66aa85226ec860a085
DESKTOP-LJH4P5G\drewj <40206862@live.napier.ac.uk>
2017-02-23 22:28:53 +0000

    Final commit for the night, finished chair, added shelf.

--- labs/coursework/src/main.cpp
+++ labs/coursework/src/main.cpp
@@ -13,6 +13,7 @@ free_camera cam;
 map<string, mesh> table;
 map<string, mesh> room;
 map<string, mesh> chair;
+map<string, mesh> shelf;
 
 map<string, texture> textures;
 double cursor_x = 0.0;
@@ -86,22 +87,62 @@ bool load_content() {
 	//Load meshes for chair
 	//front left leg of chair
 	chair["chair_front_left"] = mesh(geometry_builder::create_box(vec3(0.25f, 2.0f, 0.25f)));
-	chair["chair_front_left"].get_transform().position = vec3(6.98f, -4.0f, 3.86f);
-	chair["chair_front_left"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>()/2, vec3(0.0f, 1.0f, 0.0f));
+	chair["chair_front_left"].get_transform().position = vec3(7.375f, -4.0f, 4.125f);
 	textures["chair_front_left"] = texture("textures/woodSeat.png");
 
 	//front right leg of chair
 	chair["chair_front_right"] = mesh(geometry_builder::create_box(vec3(0.25f, 2.0f, 0.25f)));
-	chair["chair_front_right"].get_transform().position = vec3(7.645f, -4.0f, 5.47f);
-	chair["chair_front_right"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>()/2, vec3(0.0f, 1.0f, 0.0f));
+	chair["chair_front_right"].get_transform().position = vec3(7.375f, -4.0f, 5.875f);
 	textures["chair_front_right"] = texture("textures/woodSeat.png");
 
+	//back left of chair
+	chair["chair_back_left"] = mesh(geometry_builder::create_box(vec3(0.25f, 2.0f, 0.25f)));
+	chair["chair_back_left"].get_transform().position = vec3(5.625f, -4.0f, 4.125f);
+	textures["chair_back_left"] = texture("textures/woodSeat.png");
+
+	//back right of chair
+	chair["chair_back_right"] = mesh(geometry_builder::create_box(vec3(0.25f, 2.0f, 0.25f)));
+	chair["chair_back_right"].get_transform().position = vec3(5.625f, -4.0f, 5.875f);
+	textures["chair_back_right"] = texture("textures/woodSeat.png");
 
 	//Seat of the chair
 	chair["chair_seat"] = mesh(geometry_builder::create_box(vec3(2.0f, 0.25f, 2.0f)));
 	chair["chair_seat"].get_transform().position = vec3(6.5f, -2.875f, 5.0f);
-	chair["chair_seat"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>() / 2, vec3(0.0f, 1.0f, 0.0f));
 	textures["chair_seat"] = texture("textures/woodSeat.png");
+
+	//back of the chair
+	//back left flat
+	chair["chair_side_left"] = mesh(geometry_builder::create_box(vec3(0.25f, 3.0f, 0.25f)));
+	chair["chair_side_left"].get_transform().position = vec3(5.625f, -1.25f, 4.125f);
+	textures["chair_side_left"] = texture("textures/woodSeat.png");
+
+	//back right flat
+	chair["chair_side_right"] = mesh(geometry_builder::create_box(vec3(0.25f, 3.0f, 0.25f)));
+	chair["chair_side_right"].get_transform().position = vec3(5.625f, -1.25f, 5.875f);
+	textures["chair_side_right"] = texture("textures/woodSeat.png");
+
+	//top of chair
+	chair["chair_top"] = mesh(geometry_builder::create_box(vec3(0.25f, 1.5f, 1.5f)));
+	chair["chair_top"].get_transform().position = vec3(5.625f, -0.75f, 5.0f);
+	textures["chair_top"] = texture("textures/woodSeat.png");
+
+
+	//Shelf
+	//Just the shelf
+	shelf["shelf"] = mesh(geometry_builder::create_box(vec3(2.0f, 0.25f, 14.0f)));
+	shelf["shelf"].get_transform().position = vec3(-9.125f, 0.0f, 0.0f);
+	textures["shelf"] = texture("textures/shelf.jpg");
+
+
+
+	/*
+	//Not rotating for now, will fix later if rotation becomes less of an issue
+	chair["chair_front_left"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>()/2, vec3(0.0f, 1.0f, 0.0f));
+	chair["chair_front_right"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>()/2, vec3(0.0f, 1.0f, 0.0f));
+	chair["chair_back_left"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>() / 2, vec3(0.0f, 1.0f, 0.0f));
+	chair["chair_seat"].get_transform().orientation = rotate(mat4(1.0f), quarter_pi<float>() / 2, vec3(0.0f, 1.0f, 0.0f));
+
+	*/
 
 	// Load in shaders
 	eff.add_shader("shaders/basic.vert", GL_VERTEX_SHADER);
@@ -209,6 +250,25 @@ bool render() {
 
 	//render the chair
 	for (auto &e : chair) {
+		auto m = e.second;
+		// Bind effect
+		renderer::bind(eff);
+		// Create MVP matrix
+		auto M = m.get_transform().get_transform_matrix();
+		auto V = cam.get_view();
+		auto P = cam.get_projection();
+		auto MVP = P * V * M;
+		// Set MVP matrix uniform
+		glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+
+		//bind textures to renderer and render correct walls
+		renderer::bind(textures[e.first], 0);
+		glUniform1i(eff.get_uniform_location("tex"), 0);
+		//Render the object
+		renderer::render(m);
+	}
+
+	for (auto &e : shelf) {
 		auto m = e.second;
 		// Bind effect
 		renderer::bind(eff);
